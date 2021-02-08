97622f8ef29c4df6d0bf2f3c660fcacd9e35778a
Padmaraj Nidagundi <padmaraj.nidagundi@gmail.com>
2021-02-08 17:57:53 +0200

    Create test.js
    New code

---
+++ src/data-structures/basic-javascript/test.js
@@ -0,0 +1,38 @@
+//Hello world
+var msg = 'Hello World';
+console.log(msg);
+
+
+//Addation of 2 numbers
+var x = 5;
+var y = 2;
+var z = x + y;
+console.log(z);
+
+
+//Addation of 2 Sting
+var txt1 = "What a very ";
+txt1 += "nice day";
+console.log(txt1);
+
+
+//JavaScript Arrays
+var cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+
+
+//Accessing the First Array Element
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+var first = fruits[0];
+console.log(first);
+
+//Accessing the Last Array Elemen
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+var last = fruits[fruits.length - 1];
+console.log(last);
+
+//Adding Array Elements
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Lemon");
+console.log(fruits);
+
